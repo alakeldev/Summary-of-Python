3f869909d56334eb4dc6edf1a55667770f6608d6
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-07-13 23:29:39 +0200

    Creat First python file under first name and start the introduction to python

---
+++ first.py
@@ -0,0 +1,65 @@
+"""
+what is Python?
+It's Programming language that can do anything.
+Free and Open Source (use it free and also update it in future too).
+Iterpreted (Code will process in the run time and you gonna see the output direct) no need to make compiled to see the output.
+Interactive (You gonna see the output inside the terminal)
+
+
+Why Python:
+- Easy to Install (inside your software to can use it).
+- Clean and Easy (easy to learn and clean to type it no need ; only in special cases).
+- Debugging is Easy (Debugger Created with Python).
+- Cross Platfrom (you can develop in through all softwares such as: windows-Mac-Linux).
+- Expressive (So you can understand it with easy in english).
+- OOP (Object Oriented Programming).
+- Integrated (You can Build App through two languages integrate more than language python with another programming language).
+- Support Module and Packages (You can add Module and Package to you software to add it to app)
+- Large Set of Libraries and Plugins.
+- Memory Management (Garbage Collection: if you have any variable or data forget it inside the memory with python these useless Variables
+and data will remove it and collect it together to make the memory empty and clean).
+- Multi Purpos (it has many use cases and very good language).
+- Has Great Community(its document very good and you can find it easy).
+- Growing very fast.
+- Yo can switch careers with the basic knowledge.
+
+
+What Can I Do With Python?
+- Web Development (Django, Flask).
+- Games (pyGame).
+- DeskTop Apps (PyGUI, Tkinter).
+- Hacking - Network.
+- Machine Learning & Data Science.
+- AI & Robots.
+- Automation(Testing and Automate specific task).
+- Web Scrapping (Harvest) Website to show last prices for coins ,gold, cypto.
+- Andriod Games (with Frameworks)
+
+
+Some Apps Created With Python:
+- Disqus
+- Instagram
+- Spotify
+- Dropbox
+- Uber
+
+Some Companies using Python:
+- Facebook
+- Google
+- Netflix
+
+Best IDEs:
+- VS-code
+- PyCharm
+
+
+Comment in python:  Ctrl + # (Shortcut)
+"""
+
+print("Hello Python, my first application")
+print("Hello Django")
+
+# the use case of ; inside python if you want to write more than order in oneline
+print("Hello Python"); print("Hello Django")
+
+
